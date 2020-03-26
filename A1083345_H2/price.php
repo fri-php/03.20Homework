a4dcbc342332c02a06ca521d9e354582604d430d
AllEnglish <61971203+AllEnglish@users.noreply.github.com>
2020-03-26 20:07:31 +0800

    Add files via upload

---
+++ A1083345_H2/price.php
@@ -0,0 +1,143 @@
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <title>A1083345 票價資訊</title>
+
+</head>
+
+<body bgcolor="#DFDFDF">
+
+  <br/>
+
+  <h2 align="center" style="background-color: #4B4C4E">
+    <font color="white">票價表</font>
+  </h2>
+
+  <font face="微軟正黑體">
+    <table bgcolor="black" align="center">
+      <tr align="center" bgcolor="#FFE499" height="40">
+        <th width="80">站名</th>
+        <th width="80">管理學院</th>
+        <th width="80">法學院</th>
+        <th width="80">圖資</th>
+        <th width="80">理學院</th>
+        <th width="80">學二宿舍</th>
+        <th width="80">工學院</th>
+        <th width="80">行政大樓</th>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">管理學院</th>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+        <td bgcolor="#E1EED9">200</td>
+        <td bgcolor="#E1EED9">300</td>
+        <td bgcolor="#E1EED9">400</td>
+        <td bgcolor="#E1EED9">500</td>
+        <td bgcolor="#E1EED9">600</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">法學院</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+        <td bgcolor="#E1EED9">200</td>
+        <td bgcolor="#E1EED9">300</td>
+        <td bgcolor="#E1EED9">400</td>
+        <td bgcolor="#E1EED9">500</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">圖資</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="#D9E1F3">200</td>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+        <td bgcolor="#E1EED9">200</td>
+        <td bgcolor="#E1EED9">300</td>
+        <td bgcolor="#E1EED9">400</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">理學院</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="#D9E1F3">200</td>
+        <td bgcolor="#D9E1F3">300</td>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+        <td bgcolor="#E1EED9">200</td>
+        <td bgcolor="#E1EED9">300</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">學二宿舍</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="#D9E1F3">200</td>
+        <td bgcolor="#D9E1F3">300</td>
+        <td bgcolor="#D9E1F3">400</td>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+        <td bgcolor="#E1EED9">200</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">工學院</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="#D9E1F3">200</td>
+        <td bgcolor="#D9E1F3">300</td>
+        <td bgcolor="#D9E1F3">400</td>
+        <td bgcolor="#D9E1F3">500</td>
+        <td bgcolor="white">-</td>
+        <td bgcolor="#E1EED9">100</td>
+      </tr>
+      <tr align="center" height="40">
+        <th bgcolor="#FFE499">工學院</th>
+        <td bgcolor="#D9E1F3">100</td>
+        <td bgcolor="#D9E1F3">200</td>
+        <td bgcolor="#D9E1F3">300</td>
+        <td bgcolor="#D9E1F3">400</td>
+        <td bgcolor="#D9E1F3">500</td>
+        <td bgcolor="#D9E1F3">600</td>
+        <td bgcolor="white">-</td>
+      </tr>
+    </table>
+  </font><br/><br/>
+
+  <h2 align="center" style="background-color: #4B4C4E">
+    <font color="white">票種一覽表</font>
+  </h2>
+
+  <font face="微軟正黑體">
+    <table bgcolor="black" align="center">
+      <tr align="center" bgcolor="#FFD966" height="40">
+        <th colspan="2" width="160">票種</th>
+        <th width="200">優惠</th>
+        <th width="200">說明</th>
+        <th width="200">備註</th>
+      </tr>
+      <tr align="center" height="40" bgcolor="white">
+        <th colspan="2" bgcolor="#F4AF83">普通票/全票</th>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+      </tr>
+      <tr align="center" height="40" bgcolor="white">
+        <th rowspan="2" bgcolor="#C5DFB3">優待票</th>
+        <th bgcolor="#C5DFB3">敬老票</th>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+      </tr>
+      <tr align="center" height="40" bgcolor="white">
+        <th bgcolor="#C5DFB3">愛心票</th>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+        <td>(內容自行規劃)</td>
+      </tr>
+    </table>
+  </font><br/><br/>
+
+  <center>
+    <input type="button" value="返回訂票頁面" onclick="history.back(-1)">
+  </center>
+
+</body>
+
+</html>
