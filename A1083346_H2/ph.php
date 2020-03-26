661e0bc4f825f44ff17879275ac848744239cf37
1026ian <61969127+1026ian@users.noreply.github.com>
2020-03-26 23:50:52 +0800

    Add files via upload

---
+++ A1083346_H2/ph.php
@@ -0,0 +1,23 @@
+<?php
+
+$name=$_POST["name"];
+$gender=$_POST["gender"];
+$snum=$_POST["snum"];
+$cellphone=$_POST["cellphone"];
+$email=$_POST["email"];
+$number=$_POST["number"];
+$station=$_POST["station"];
+$YourTicket=$_POST["YourTicket"];
+$seat=$_POST["seat"];
+
+echo $name."<br/>";
+echo $gender."<br/>";
+echo $snum."<br/>";
+echo $cellphone."<br/>";
+echo $email."<br/>";
+echo $number."<br/>";
+echo $station."<br/>";
+echo $YourTicket."<br/>";
+echo $seat."<br/>";
+
+?
