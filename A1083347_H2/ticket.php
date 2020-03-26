98700ce615c3bd25b666315a216c4c42226e1103
Eleven-41 <61977870+Eleven-41@users.noreply.github.com>
2020-03-26 14:16:03 +0800

    Add files via upload

---
+++ A1083347_H2/ticket.php
@@ -0,0 +1,24 @@
+<?php
+
+$name=$_POST["name"];
+$id=$_POST["id"];
+$gender=$_POST["gender"];
+$ticket=$_POST["ticket"];
+$starting=$_POST["starting"];
+$destination=$_POST["destination"];
+$seat=$_POST["seat"];
+$number=$_POST["number"];
+$mail=$_POST["mail"];
+
+echo $name."<br/>";
+echo $id."<br/>";
+echo $gender."<br/>";
+echo $ticket."<br/>";
+echo $starting."<br/>";
+echo $destination."<br/>";
+echo $seat."<br/>";
+echo $number."<br/>";
+echo $mail."<br/>";
+echo $name."<br/>";
+
+?>
