cc238af01fabd083ac34c3fcf9ca50075c024384
Fy-CHi <a1073303@mail.nuk.edu.tw>
2020-03-26 22:49:34 +0800

    A1073303

---
+++ A1073303_H2/td.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>I know</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+$NAME = $_POST["NAME"];
+$STUDENT_ID = $_POST["STUDENT_ID"];
+$SEX = $_POST["SEX"];
+$TICKET = $_POST["TICKET"];
+$AMOUNT = $_POST["AMOUNT"];
+$TICKET = $_POST["TICKET"];
+$AMOUNT = $_POST["AMOUNT"];
+$TICKET = $_POST["TICKET"];
+$AMOUNT = $_POST["AMOUNT"];
+$START = $_POST["START"];
+$FINAL = $_POST["FINAL"];
+$SEAT_PREFER= $_POST["SEAT_PREFER"];
+$PHONE_NUMBER = $_POST["PHONE_NUMBER"];
+$EMAIL = $_POST["EMAIL"];
+$SPECIAL_NOTE = $_POST["SPECIAL_NOTE"];
+echo $NAME."<br/>";
+echo $STUDENT_ID."<br/>";
+echo $SEX."<br/>";
+echo $TICKET."<br/>";
+echo $AMOUNT."<br/>";
+echo $START."<br/>";
+echo $FINAL."<br/>";
+echo $SEAT_PREFER."<br/>";
+echo $PHONE_NUMBER."<br/>";
+echo $EMAIL."<br/>";
+echo $SPECIAL_NOTE."<br/>";
+
+?>
