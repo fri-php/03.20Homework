5254b43f9612b15ccd4bd86c611c95fcfd1d5563
alexliu0121 <45057088+alexliu0121@users.noreply.github.com>
2020-03-26 23:10:45 +0800

    Add files via upload

---
+++ a1073311_H2/a1073311HW2.php
@@ -0,0 +1,31 @@
+<html>
+</html>
+
+<?php
+
+
+$name=$_POST["name"];
+$id=$_POST["id"];
+$gender=$_POST["gender"];
+$type=$_POST["票種"];
+$number=$_POST["select_one"];
+$start=$_POST["select_one_start"];
+$stop=$_POST["select_one_stop"];
+$seat=$_POST["select_one_seat"];
+$phone=$_POST["phone"];
+$email=$_POST["email"];
+$plus=$_POST["plus"];
+echo $name."<br/>";
+echo $id."<br/>";
+echo $gender."<br/>";
+echo $type."<br/>";
+echo $number."<br/>";
+echo $start."<br/>";
+echo $stop."<br/>";
+echo $seat."<br/>";
+echo $phone."<br/>";
+echo $email."<br/>";
+echo $plus."<br/>";
+"<br/>";
+"<br/>";
+"<br/>";
