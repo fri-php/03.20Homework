60f036ec82f066028690c3dc61407ca17c0de338
1109josh <62115265+1109josh@users.noreply.github.com>
2020-03-26 23:45:17 +0800

    Add files via upload

---
+++ a1083314_HW2/hw.php
@@ -0,0 +1,29 @@
+<?php
+
+$id=$_POST["username"];
+$number=$_POST["number"];
+$gender=$_POST["sex"];
+$start=$_POST["start"];
+$end=$_POST["end"];
+$sit=$_POST["sit"];
+$phone=$_POST["phone"];
+$email=$_POST["email"];
+$address=$_POST["address"];
+$ticket1=$_POST["ticket1"];
+$ticket2=$_POST["ticket2"];
+$ticket3=$_POST["ticket3"];
+
+echo $id."<br/>";
+echo $number."<br/>";
+echo $gender."<br/>";
+echo $start."<br/>";
+echo $end."<br/>";
+echo $sit."<br/>";
+echo $phone."<br/>";
+echo $email."<br/>";
+echo $address."<br/>";
+echo $ticket1."<br/>";
+echo $ticket2."<br/>";
+echo $ticket3."<br/>";
+
+?>
