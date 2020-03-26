b3e52ce79e2d4da4ec516732c31f7adf3cf98014
chukuanhao66 <62129432+chukuanhao66@users.noreply.github.com>
2020-03-26 21:14:28 +0800

    Add files via upload

---
+++ A1083312_H2/order.php
@@ -0,0 +1,58 @@
+<body>
+<center>
+
+		<h1>~歡迎來到Nuk railway訂票網頁~</h1><br>
+		<h2>~歡迎來到Nuk railway訂票網頁~</h2><br>
+		<h3>~歡迎來到Nuk railway訂票網頁~</h3><br>
+		<h4>~歡迎來到Nuk railway訂票網頁~</h4><br>
+		<h5>~歡迎來到Nuk railway訂票網頁~</h5><br>
+		<h6>~歡迎來到Nuk railway訂票網頁~</h6><br>
+		<a href="file:///C:/Users/USER/Desktop/a1083312%E7%A5%A8%E5%83%B9%E8%B3%87%E8%A8%8A.html">
+		<img src="https://upload.cc/i1/2020/03/11/uCbne0.png"></a>
+		</br>
+<form action="result.php" method="POST" >
+		請輸入您的姓名:<input type="text" name="id"><br/>
+		請輸入你的學號:<input type="text" name="schoolnumber"><br/>
+		女<input type="radio" value="F" name="gender">
+		男<input type="radio" value="M" name="gender"><br/>
+		票數(不用請填0)<br/>
+		
+		全票<input type="text" name="adult" >
+		敬老票<input type="text" name="old" >
+		愛心票<input type="text" name="heart" ><br/>
+		起站<select name="statstart">
+			<option value="manage">管院</option>
+			<option value="law">法院</option>
+			<option value="library">圖資</option>
+			<option value="science">理院</option>
+			<option value="dorm2">學二宿舍</option>
+			<option value="engineering">工學院</option>
+			<option value="public" selected>行政大樓</option>
+			</select>
+		終站<select name="statend">
+			<option value="manage">管院</option>
+			<option value="law">法院</option>
+			<option value="library">圖資</option>
+			<option value="science">理院</option>
+			<option value="dorm2">學二宿舍</option>
+			<option value="engineering">工學院</option>
+			<option value="public" selected>行政大樓</option>
+			</select></br>
+		電話<input type="tel" name="tel"><br/>
+		座位喜好
+		<select name="seat">
+			<option value="road">走道</option>
+			<option value="middle">中間</option>
+			<option value="window"selected>靠窗</option>
+			</select></br>
+		信箱<input type="mail" name="mail"><br/> 
+		<input type= "reset" value="從新輸入"></>
+		<input type="submit" value="確定送出" onclick="location.href='file:///C:/Users/USER/Desktop/%E8%B3%BC%E8%B2%B7%E6%88%90%E5%8A%9F.html'"><br/>
+
+</center>
+</form>
+
+
+
+
+</body>>
---
+++ A1083312_H2/result.php
@@ -0,0 +1,32 @@
+<?php
+$id=$_POST["id"];
+$schoolnumber=$_POST["schoolnumber"];
+$gender=$_POST["gender"];
+
+
+
+$statstart=$_POST["statstart"];
+$statend=$_POST["statend"];
+$seat=$_POST["seat"];
+$mail=$_POST["mail"];
+
+
+$adult=$_POST["adult"];
+$old=$_POST["old"];
+$heart=$_POST["heart"];
+
+
+
+
+
+echo "id:".$id."<br>";
+echo "schoolnumber:".$schoolnumber."<br>";
+echo"gender:".$gender."<br>";
+echo "adult:".$adult."<br>";
+echo "old:".$old."<br>";
+echo "heart:".$heart."<br>";
+echo"statstart:".$statstart."<br>";
+echo "statend:".$statend."<br>";
+echo "seat:".$seat."<br>";
+echo "mail:".$mail."<br>";
+?>
