f4a96cd71b0716c570dba5187aa9af82fad4c108
piguang1112 <62090874+piguang1112@users.noreply.github.com>
2020-03-26 18:19:31 +0800

    Add files via upload

---
+++ a1083302_H2/me.php
@@ -0,0 +1,32 @@
+
+
+<?php
+
+ $name=$_POST["name"];
+ echo "姓名:".$name."<br/>";
+ $scode=$_POST["scode"];
+ echo "學號:".$scode."<br/>";
+ $sex=$_POST["sex"];
+ echo "性別:".$sex."<br/>";
+ $station=$_POST["station"];
+ echo "性別:".$station."<br/>";
+ $firststation=$_POST["firststation"];
+ echo "起訖站:".$firststation."<br/>";
+ $seat=$_POST["seat"];
+  echo "座位喜好:".$seat."<br/>";
+ $phone=$_POST["phone"];
+  echo "聯絡電話:".$phone."<br/>";
+ $email=$_POST["email"];
+  echo "信箱:".$email."<br/>";
+ $other=$_POST["other"];
+  echo "備註:".$other."<br/>";
+ 
+ 
+ 
+ 
+ 
+
+ 
+
+
+?>
